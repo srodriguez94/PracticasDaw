c2e5046af461dc33f98d87d9f8446f193f1172b4
el_19 <el_19@DESKTOP-RNGFDGB>
2017-10-05 19:24:38 +0200

    Hasta funcion1

---
+++ src/pruebagithub/PruebaGitHub.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebagithub;
+
+/**
+ *
+ * @author el_19
+ */
+public class PruebaGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // TODO code application logic here
+    }
+    
+    public void funcio1() {
+        
+    }
+  
+}
